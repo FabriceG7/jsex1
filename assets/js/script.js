de4d1673677887a6a4b793e21145c3cea27c55bd
FabriceG7 <goret.goret@sfr.fr>
2020-05-12 14:51:04 +0200

    ex1partie2

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+function changeImage(){
+  document.getElementById('banana').src ='assets/image/banane 2.jpg';
+}
+function originImage(){
+  document.getElementById('banana').src ='assets/image/banane 1.jpg';
+}
